d736f7759920ef288d9a9593d18dd5b1930c92e4
Khaled Yakdan <yakdan@code-intelligence.com>
2020-11-26 08:19:10 +0100

    Allocate enough memory for the ean decoder

--- backend/upcean.c
+++ backend/upcean.c
@@ -696,8 +696,8 @@ INTERNAL void ean_leading_zeroes(struct zint_symbol *symbol, unsigned char sourc
 /* splits string to parts before and after '+' parts */
 INTERNAL int eanx(struct zint_symbol *symbol, unsigned char source[], int src_len) {
     unsigned char first_part[20] = {0}, second_part[20] = {0}, dest[1000] = {0};
-    unsigned char local_source[20] = {0};
-    //unsigned char local_source[27] = {0};
+    //unsigned char local_source[20] = {0};
+    unsigned char local_source[27] = {0};
     /*
      * Exchange the two lines above to fix CVE-2020-9385,
      * See https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-9385
