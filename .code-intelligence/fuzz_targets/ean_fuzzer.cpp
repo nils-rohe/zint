0b65beebea2e75452254feb155fbd3a281ec7608
janschrewe <schrewe@code-intelligence.com>
2021-03-29 16:30:08 +0200

    Improve fuzz target and used SymCC

--- .code-intelligence/fuzz_targets/ean_fuzzer.cpp
+++ .code-intelligence/fuzz_targets/ean_fuzzer.cpp
@@ -7,6 +7,7 @@
 #include "common.h"
 #include "gs1.h"
 #include <unistd.h>
+#include <fuzzer/FuzzedDataProvider.h>
 
 int ZBarcode_Buffer(struct zint_symbol *symbol, int rotate_angle);
 
@@ -15,12 +16,23 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
   if (Size < 4)
     return 0;
 
+
+  FuzzedDataProvider fdp( Data, Size );
+
+
+  int sym_number = fdp.ConsumeIntegral<int>();
+  if (sym_number < 0){
+    return 0;
+  }
+
   struct zint_symbol *my_symbol;
   my_symbol = ZBarcode_Create();
-
-  my_symbol->symbology = BARCODE_EANX;
-  unsigned char *input = (unsigned char *)Data;
-  ZBarcode_Encode(my_symbol, input, Size);
+  my_symbol->symbology= sym_number;
+  //std::cout << "symbology= " << my_symbol->symbology << "\n";
+  std::string input = fdp.ConsumeRandomLengthString();
+  //std::cout << "input= " << input << "\n";
+  const unsigned char* input_cstr = (const unsigned char *) input.c_str();
+  ZBarcode_Encode(my_symbol, input_cstr, input.length());
   ZBarcode_Delete(my_symbol);
 
   return 0;
