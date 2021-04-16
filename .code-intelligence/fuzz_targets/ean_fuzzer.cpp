0b90a65b1f1cd06e40735584cb0832583a26f745
Moe <qasem@code-intelligence.de>
2021-04-16 11:58:55 +0200

    revert fix change

--- .code-intelligence/fuzz_targets/ean_fuzzer.cpp
+++ .code-intelligence/fuzz_targets/ean_fuzzer.cpp
@@ -12,5 +12,16 @@ int ZBarcode_Buffer(struct zint_symbol *symbol, int rotate_angle);
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
+  if (Size < 4)
+    return 0;
+
+  struct zint_symbol *my_symbol;
+  my_symbol = ZBarcode_Create();
+
+  my_symbol->symbology = BARCODE_EANX;
+  unsigned char *input = (unsigned char *)Data;
+  ZBarcode_Encode(my_symbol, input, Size);
+  ZBarcode_Delete(my_symbol);
+
   return 0;
 }
